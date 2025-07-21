253e9c5cf6f4d315ae1f1d09053eb8d06f87c53b
ramcodeverse <ramcodeverse@gmail.com>
2025-07-21 10:07:00 +0530

    Add .next/static/chunks/src_app_simulator_page_tsx_a993527f._.js 1753072617922

---
+++ .next/static/chunks/src_app_simulator_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_93f5c376._.js",
+  "static/chunks/node_modules_c3309b00._.js"
+],
+    source: "dynamic"
+});
